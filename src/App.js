f71ea98d36b39ebc3dead138c36bd3e7e27990e0
Randy <99023957+Randy-Hwang@users.noreply.github.com>
2022-07-14 17:45:43 +0900

    studying about styled-components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import styled, { keyframes } from "styled-components";
+
+// 2.5부터
+
+const Wrapper = styled.div`
+  display: flex;
+`;
+
+const rotation = keyframes`
+  0% {
+    transform: rotate(0deg);
+    border-radius: 0px;
+  } 50% {
+    border-radius: 100px;
+  } 100% {
+    transform: rotate(360deg);
+    border-radius: 0px;
+  }
+`;
+
+const Box = styled.div`
+  height: 100px;
+  width: 100px;
+  background-color: tomato;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation: ${rotation} 1s linear infinite;
+  // 자식 요소를 이런 방식으로 선택할 수도 있다.
+  span {
+    font-size: 36px;
+    &:hover {
+      // = span:hover. 이런 방식으로 수도 선택자도 가능하다.
+      font-size: 40px;
+    }
+    &:active {
+      // 클릭하고있으면 active, 아니면 inactive
+      opacity: 0;
+    }
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Box>
+        <span>🥰</span>
+      </Box>
+    </Wrapper>
   );
 }
 
