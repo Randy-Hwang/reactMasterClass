8f5f46dacd0cdd8a0bbada922427d3595df84edf
Randy <99023957+Randy-Hwang@users.noreply.github.com>
2022-07-17 17:20:26 +0900

    updated

--- src/App.js
+++ src/App.js
@@ -1,46 +1,24 @@
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
 
-// 2.5부터
+// 3.0부터
 
 const Wrapper = styled.div`
   display: flex;
-`;
-
-const rotation = keyframes`
-  0% {
-    transform: rotate(0deg);
-    border-radius: 0px;
-  } 50% {
-    border-radius: 100px;
-  } 100% {
-    transform: rotate(360deg);
-    border-radius: 0px;
-  }
-`;
-
-const Emoji = styled.span`
-  font-size: 36px;
-`;
-
-const Box = styled.div`
-  height: 100px;
-  width: 100px;
-  background-color: tomato;
-  display: flex;
+  height: 100vh;
+  width: 100vw;
   justify-content: center;
   align-items: center;
-  animation: ${rotation} 1s linear infinite;
-  ${Emoji} :hover {
-    font-size: 96px;
-  }
+  background-color: ${(props) => props.theme.backgroundColor};
+`;
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <Emoji>🥰</Emoji>
-      </Box>
+      <Title>Hello</Title>
     </Wrapper>
   );
 }
